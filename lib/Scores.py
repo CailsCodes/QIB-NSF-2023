7b14a485b0656f8b5a169d5aece5eb028889192a
Cailean Carter <cailean.carter@hotmail.com>
2022-12-18 16:46:31 +0000

    created scores class

--- lib/Scores.py
+++ lib/Scores.py
@@ -1,36 +1,36 @@
 import sqlite3
 from itertools import count
 
+class Scores:
 
-SQL_MAKE_TABLE = """CREATE TABLE IF NOT EXISTS scores (
-    id INTEGER PRIMARY KEY,
-    time_stamp TEXT NOT NULL,
-    player INTEGER NOT NULL,
-    test_name TEXT NOT NULL,
-    cut_off TEXT NOT NULL,
-    stage_of_disease TEXT NOT NULL,
-    outcome INTEGER NOT NULL,
-    time_to_complete INTEGER NOT NULL,
-    num_scans INTEGER NOT NULL
-    )"""
+    def __init__(self, fp="scores.db"):
+        self.SQL_MAKE_TABLE = """CREATE TABLE IF NOT EXISTS scores (
+            id INTEGER PRIMARY KEY,
+            time_stamp TEXT NOT NULL,
+            player INTEGER NOT NULL,
+            test_name TEXT NOT NULL,
+            cut_off TEXT NOT NULL,
+            stage_of_disease TEXT NOT NULL,
+            outcome INTEGER NOT NULL,
+            time_to_complete INTEGER NOT NULL,
+            num_scans INTEGER NOT NULL
+            )"""
 
-_db = sqlite3.connect('scores.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-
-scores = _db.cursor()
-scores.execute(SQL_MAKE_TABLE)
-
-_id = count(start=get_max_id())
-
-
-def get_max_id():
-    try:
-        result = scores.execute("SELECT MAX(id) FROM scores")
-    except:
-        result = 0
-    return int(result)
+        self._db = sqlite3.connect(fp, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        self.scores = self._db.cursor()
+        self.scores.execute(SQL_MAKE_TABLE)
+        self._id = count(start=get_max_id())
 
 
-def add_results(*args):
-    scores.execute("INSERT INTO scores(?,?,?,?,?,?,?,?,?)", args)
-    scores.commit()
+    def get_max_id(self):
+        try:
+            result = self.scores.execute("SELECT MAX(id) FROM scores")
+        except:
+            result = 0
+        return int(result)
+
+
+    def add_results(self, *args):
+        self.scores.execute("INSERT INTO scores(?,?,?,?,?,?,?,?,?)", args)
+        self.scores.commit()
 
